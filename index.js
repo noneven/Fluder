65656d16b9729bb549162085cf9db0b82911cfe9
chenjian03 <chenjian03@58ganji.com>
2016-08-31 20:43:45 +0800

    rename fluder some private function and variable

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 /**
- * 队列
+ * Fluder-A unidirectional data flow tool based on flux.
+ */
+
+/**
+ * workflow Queue
  */
 import Queue from './queue';
+
 /**
- * 事件
+ * custom Event
  */
 import EventEmitter from 'events';
+
 /**
  * Fluder
  */
@@ -16,54 +22,64 @@ export default class Fluder {
      */
     constructor() {
         /**
-         * store handler 注册Map
+         * store handlers 注册Map
          * @type {Object}
          */
         this._registers = {};
+
         /**
          * dispatch栈
          * @type {Array}
          */
         this._dispatchStack = [];
+
         /**
          * 初始化
          */
-        this.init();
+        this._init();
     }
-    init(){
+
+    _init() {
         /**
          * 中间件，集中处理action payload和storeId
          */
-        this._middlewareQueue = new Queue((payload)=>{
+        this._middleware = new Queue((payload) => {
             /**
              * 中间件队列执行完后触发Store handler的调用
              */
-            this.__invoke__(payload);
+            this._invoke(payload);
 
         }, true);
     }
+
     /**
-    * Fluder Store唯一ID
-    */
-    unique(){
-       return 'Fluder_Store_'+ Math.round(Math.random() * 100000)
+     * Fluder Store唯一ID
+     */
+    _unique() {
+        return 'Fluder_Store_' + Math.round(Math.random() * 100000)
     }
+
     /**
      * action对handler的调用(内部调用)
      * @param  {object} payload 此时的payload包含action和action对应的storeId
      * @return {void}           无返回值
      */
-    __invoke__(payload) {
+    _invoke(payload) {
         /**
          * storeId: 用于map到register里面注册的handler
          * @type {string}
          */
-        let {storeId} = payload;
+        let {
+            storeId
+        } = payload;
         /**
          * store和它对应的handler
          * @type {object}
          */
-        let {store, handlers} = this._registers[storeId];
+        let {
+            store,
+            handlers
+        } = this._registers[storeId];
 
         /**
          * action payload
@@ -89,7 +105,7 @@ export default class Fluder {
              * 这里把payload传给change的Store，可以做相应的渲染优化[局部渲染]
              */
             // if (result !== undefined) {
-                store.emitChange(result, payload);
+            store.emitChange(result, payload);
             // }
         }
     }
@@ -100,17 +116,17 @@ export default class Fluder {
      * @param  {object} action  action数据
      * @return {void}           无返回值
      */
-    __dispatch__(storeId, payload) {
+    _dispatch(storeId, payload) {
         /**
          * 在当前Action触发的Store handler回调函数中再次发起了当前Action，这样会造成A-A循环调用,出现栈溢出
          */
-        if(this._currentDispatch == storeId){
+        if (this._currentDispatch == storeId) {
             throw Error('action __invoke__ self!')
         }
         /**
          * 在当前Action触发的Store handler回调函数中再次触发了当前Action栈中的Action，出现A-B-C-A式循环调用，也会出现栈溢出
          */
-        if(this._dispatchStack.indexOf(storeId)!=-1){
+        if (this._dispatchStack.indexOf(storeId) != -1) {
             throw Error('action __invoke__ to a circle!');
         }
         /**
@@ -126,7 +142,7 @@ export default class Fluder {
         /**
          * 发出action的时候 统一走一遍中间件
          */
-        this._middlewareQueue.execute({
+        this._middleware.execute({
             storeId,
             payload
         });
@@ -135,23 +151,26 @@ export default class Fluder {
          */
         this._endDispatch()
     }
+
     /**
      * 更新Action栈以及记录当前ActionID
      */
-    _startDispatch(storeId){
-        this._dispatchStack||(this._dispatchStack=[]);
+    _startDispatch(storeId) {
+        this._dispatchStack || (this._dispatchStack = []);
         this._dispatchStack.push(storeId);
         this._currentDispatch = storeId;
     }
+
     /**
      * Action执行完更新Action栈以及删除当前ActionID
      */
-    _endDispatch(){
+    _endDispatch() {
         this._dispatchStack.pop();
         this._currentDispatch = null;
     }
+
     /**
-     * 创建action
+     * 创建action[对外API]
      * @param  {string} storeId  该action作用于那个store,和store的storeId一一对应
      * @param  {object} actionCreators 需要创建的action对象
      * @return {object} 返回一个actions对象,具有调用action触发store change的能力
@@ -168,8 +187,7 @@ export default class Fluder {
             console.warn('action handler\'s length is 0, need you a action handler?');
         }
 
-        let creator;
-        let actions = {};
+        let creator, actions = {};
         /**
          * 遍历创建Action
          */
@@ -183,7 +201,7 @@ export default class Fluder {
                     /**
                      * action里面发出改变store消息
                      */
-                    return this.__dispatch__(storeId, creator(...arguments));
+                    return this._dispatch(storeId, creator(...arguments));
                 }.bind(this);
             }.call(this, storeId, creator);
         }
@@ -191,7 +209,7 @@ export default class Fluder {
     }
 
     /**
-     * 创建store
+     * 创建store[对外API]
      * @param  {string} storeId  该store的唯一标识，和action里的storeId一一对应
      * @param  {object} method   操作store的api(一般提供get set del update等)
      * @param  {object} handlers action的处理回调对象，handler的key需要和actionType一致
@@ -206,16 +224,16 @@ export default class Fluder {
         }
         const CHANGE_EVENT = 'change';
         /**
-        * 创建store，继承EventEmitter
-        */
+         * 创建store，继承EventEmitter
+         */
         let store = Object.assign(method, EventEmitter.prototype, {
             /**
              * 统一Store的EventEmitter调用方式，避免和全局EventEmitter混淆
              * 这里把payload传给change的Store，可以做相应的渲染优化[局部渲染]
              * 这里的局部优化是指全局Stores更新，触发的Store Handler较多，可以通过payload的数据过滤
              */
-            emitChange: function(result,payload) {
-                this.emit(CHANGE_EVENT,result,payload);
+            emitChange: function(result, payload) {
+                this.emit(CHANGE_EVENT, result, payload);
             },
             addChangeListener: function(callback) {
                 this.on(CHANGE_EVENT, callback);
@@ -223,18 +241,22 @@ export default class Fluder {
             removeChangeListener: function(callback) {
                 this.removeListener(CHANGE_EVENT, callback);
             },
-            removeAllChangeListener: function(){
+            removeAllChangeListener: function() {
                 this.removeAllListener()
             }
         });
         /**
          * 注册到register MAP里面
          */
-        this._registers[storeId] = {store, handlers};
+        this._registers[storeId] = {
+            store,
+            handlers
+        };
         return store;
     }
 
     /**
+     * 创建ActionStore[对外API]
      * 提供一个方法同时创建action和store，可以有效避免action和store的ID不统一的情况
      * @param  {object} actionCreators 创建action需要用到的action对象
      * @param  {object} method         创建store需要的store操作api
@@ -242,7 +264,10 @@ export default class Fluder {
      * @param  {string} storeId        action和store统一的唯一标识
      * @return {object}                返回一个action和store对象
      */
-    actionStoreCreate(actionCreators, method, handlers, storeId = this.unique()) {
+    actionStoreCreate(actionCreators, method, handlers, storeId = this._unique()) {
+        /**
+         * 返回值为Store+Action
+         */
         return {
             action: this.actionCreate(storeId, actionCreators),
             store: this.storeCreate(storeId, method, handlers)
@@ -250,7 +275,7 @@ export default class Fluder {
     }
 
     /**
-     * 中间件
+     * 中间件[对外API]
      * @param  {function} middleware action统一流入中间件
      * 这里和redux类似，和express等框架对请求的处理一样
      */
@@ -261,16 +286,16 @@ export default class Fluder {
              * 需要排队等到所有的中间件 完成才会触发对应的handler
              * @param  {function} middleware
              */
-            this._middlewareQueue.enter(middleware);
+            this._middleware.enter(middleware);
         }
-        if(({}).toString.call(middleware)==='[object Array]'){
+        if (({}).toString.call(middleware) === '[object Array]') {
             for (let i = 0; i < middleware.length; i++) {
                 if (typeof middleware === 'function') {
                     this.applyMiddleware(middleware[i])
                 }
             }
         }
-        //支持链式中间件
+        //链式中间件
         return this;
     }
 }
