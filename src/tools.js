5119e5807b8d49cf574f974fe3faf007998d88ca
chenjian03 <chenjian03@58ganji.com>
2016-09-07 17:23:32 +0800

    added "catchError tool" for @_invoke

--- src/tools.js
+++ src/tools.js
@@ -1,6 +1,22 @@
 export function unique(){
-	  /**
+	/**
      * Fluder Store唯一ID
      */
-    return '@@Fluder/StoreId/' + Math.random().toString(36).substring(7).split('').join('.');
+    return '@@Fluder/StoreId/' +
+		Math.random()
+		.toString(36)
+		.substring(7)
+		.split('')
+		.join('.');
+}
+export function catchError(e){
+	let start = '\n\n@@Fluder/Start\n';
+	let end = '\n@@Fluder/End\n\n';
+
+	throw Error(start +
+		'Error: ' +
+		e.line + "行" +
+		e.column + '列' +
+		e.message +
+		end);
 }
