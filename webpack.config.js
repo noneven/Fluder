daf1e182015bead6e423e74d27e1f606c6f78625
coderwin <chenjiancj2011@outlook.com>
2016-09-23 21:00:40 +0800

    config fine-tune

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,35 @@
-
-var webpack = require('webpack');
-
 module.exports = {
-    devtool: 'source-map',
-    entry: {
-        "index": "./example/todoReact/index.js",
-    },
-    output: {
-        path: __dirname + "/build/js",
-        filename: "[name].bundle.js",
-    },
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            query: {
-                presets: [ 'es2015', 'react' ]
-            }
-        },{test: /\.css$/, loader: 'style-loader!css-loader'}]
-    },resolve: {
-        extensions: ['', '.js', '.jsx']
-    },plugins: [
-        //压缩
-        // new webpack.optimize.UglifyJsPlugin({
-        //     compress: {
-        //         warnings: false
-        //     }
-        // }),
-    ]
-};
+  devtool: 'source-map',
+  entry: {
+    'index': './example/todoReact/index.js'
+  },
+  output: {
+    path: __dirname + '/build/js',
+    filename: '[name].bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    /*
+     * 压缩
+     */
+    // new webpack.optimize.UglifyJsPlugin({
+    //     compress: {
+    //         warnings: false
+    //     }
+    // }),
+  ]
+}
